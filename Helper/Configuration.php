164b6026f0c83bcfacde427bcb622053dbdcffb3
Piotr Matras <piotr.matras@creativestyle.pl>
2021-03-26 13:53:20 +0000

    Revert "Merge branch 'feat-add-to-cart-recaptcha' into '1.x'"
    This reverts merge request !4

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -1,35 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 namespace MageSuite\ExtendedRecaptcha\Helper;
 
 class Configuration extends \Magento\Framework\App\Helper\AbstractHelper
 {
     const RECAPTCHA_CUSTOM_NOTE_ENABLED = 'recaptcha_frontend/type_for/custom_note_enabled';
+
     const RECAPTCHA_CUSTOM_NOTE_TEXT = 'recaptcha_frontend/type_for/custom_note_text';
-    const RECAPTCHA_ADD_TO_CART_PROTECTION_MODE = 'recaptcha_frontend/type_for/add_to_cart_protection_mode';
 
-    public function isRecaptchaCustomNoteEnabled($storeId = null): bool
+    public function isRecaptchaCustomNoteEnabled()
     {
-        return $this->scopeConfig->isSetFlag(
-            self::RECAPTCHA_CUSTOM_NOTE_ENABLED,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $storeId
-        );
+        return $this->scopeConfig->getValue(self::RECAPTCHA_CUSTOM_NOTE_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
-    public function getRecaptchaCustomNote($storeId = null): string
+    public function getRecaptchaCustomNote()
     {
-        return (string)$this->scopeConfig->getValue(
-            self::RECAPTCHA_CUSTOM_NOTE_TEXT,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $storeId
-        );
-    }
-
-    public function getAddToCartProtectionMode(): string
-    {
-        return (string)$this->scopeConfig->getValue(self::RECAPTCHA_ADD_TO_CART_PROTECTION_MODE);
+        return $this->scopeConfig->getValue(self::RECAPTCHA_CUSTOM_NOTE_TEXT, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 }

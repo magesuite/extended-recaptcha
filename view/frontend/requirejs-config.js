aababc53c7b1668f647d0f7cd4e971da7c127824
Piotr Matras <piotr.matras@creativestyle.pl>
2021-03-31 12:48:28 +0000

    Feat reset after atc

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_ReCaptchaFrontendUi/js/reCaptcha': {
+                'MageSuite_ExtendedRecaptcha/js/reCaptcha-mixin': true
+            }
+        }
+    }
+};
